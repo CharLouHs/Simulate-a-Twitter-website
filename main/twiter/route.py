2d55423d23d2033b31855a175d6c88578267f9df
CharLouHs <chacptzy258@gmail.com>
2022-04-23 17:17:15 +0800

    Flask migration

---
+++ main/twiter/route.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template,redirect, url_for
+from twiter.forms import LoginForm
+from twiter.models import User
+from twiter import db
+
+def index():
+    name={'username':'root'}
+    posts=[
+        {'author':{'name':'Mary'},'body':27}
+        
+
+    ]
+    return render_template('index.html',name=name,posts=posts)
+
+def login():
+    form=LoginForm(WTF_CSRF_ENABLED = False)
+   
+    if form.validate_on_submit():
+        msg=('username={},password={},remem_me={}'.format (
+            form.username.data,
+            form.password.data,
+            form.remem_me.data
+        ))
+        print(msg)
+        return redirect(url_for('index'))
+    else:
+        print(form.errors)
+    
+    return render_template('login.html',title='Login',form=form)
